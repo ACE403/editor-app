579907fd7e9d2216fb9fadaba487989783a8ba2b
Ace <devangpateloo7o@gmail.com>
2022-12-20 02:09:15 +0530

    ongoing

--- app/src/main/java/com/example/editor/MainActivity.kt
+++ app/src/main/java/com/example/editor/MainActivity.kt
@@ -1,36 +1,52 @@
 package com.example.editor
 
 import android.content.Intent
-import android.graphics.Canvas
+
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.text.TextUtils
+
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ImageButton
-import com.example.editor.MyCanvasView
-import com.example.editor.MyCanvasView.Companion.colorList
+
+import androidx.core.content.res.ResourcesCompat
+
 import com.example.editor.MyCanvasView.Companion.currentBrush
-import com.example.editor.MyCanvasView.Companion.pathList
 
-import com.example.editor.R
-
+import com.example.editor.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
     companion object {
 
         var path=Path()
         var paintbrush = Paint()
         var tool=0
     }
+
+
+//        val adapter = ArrayAdapter(
+//            this, android.R.layout.simple_spinner_dropdown_item, arrayListOf("all types","assignment","exam")
+//        )
+//        binding.autoCompleteTextView.setAdapter(adapter)
+////        binding.autoCompleteTextView.setText("All Types")
+//        binding.autoCompleteTextView.setDropDownBackgroundDrawable(
+//            ResourcesCompat.getDrawable(
+//                resources,
+//                R.drawable.filter_spinner_dropdown_bg,
+//                null
+//            )
+//        )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
          val pen = findViewById<ImageButton>(R.id.pen)
@@ -40,6 +56,11 @@ class MainActivity : AppCompatActivity() {
         val blue=findViewById<ImageButton>(R.id.blue_color)
         val yellow=findViewById<ImageButton>(R.id.yellow_color)
         val goto=findViewById<ImageButton>(R.id.link)
+
+        val mycolors= listOf("india","china","usa")
+//        val feelings=resources.getStringArray(R.array.feelings)
+        val adapter = ArrayAdapter(this,R.layout.dropdown_item,mycolors)
+        binding.autoCompleteTextView.setAdapter(adapter)
 
         goto.setOnClickListener{
             var browserIntent =  Intent(Intent.ACTION_VIEW, Uri.parse("http://localhost:8080/"))
@@ -95,6 +116,11 @@ class MainActivity : AppCompatActivity() {
     private fun currentColor(color: Int) {
         currentBrush=color
         path= Path()
+    }
+    fun AutoCompleteTextView.showDropdown(adapter: ArrayAdapter<String>?) {
+        if(!TextUtils.isEmpty(this.text.toString())){
+            adapter?.filter?.filter(null)
+        }
     }
 
 
